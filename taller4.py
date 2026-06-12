a512c45b093a65a6cafc3aa79864dba1264a1621
Jose manuel espitia romero <espitiaromerojosemanuel91@gmail.com>
2026-06-12 18:39:57 -0500

    Fix spelling and accentuation in comments and messages7

--- taller4.py
+++ taller4.py
@@ -1,4 +1,4 @@
-# Diccionario con factores de conversión
+# Diccionario con factores de conversion
 conversiones = {
     "metros_pies": 3.28084,
     "pies_metros": 0.3048,
@@ -8,15 +8,15 @@ conversiones = {
     "lb_kg": 0.453592
 }
 
-# Función para convertir unidades
+# Funcion para convertir unidades
 def convertir(cantidad, origen, destino):
-    # .lower() convierte el texto a minúsculas para evitar errores
+    # .lower() convierte el texto a minusculas para evitar errores
     clave = origen.lower() + "_" + destino.lower()
 
     if clave in conversiones:
         return cantidad * conversiones[clave]
     else:
-        return "Conversión no disponible."
+        return "Conversion no disponible."
 
 # Programa principal
 try:
@@ -28,4 +28,5 @@ try:
     print("Resultado:", resultado)
 
 except ValueError:
-    print("Error: Por favor, ingrese un número válido para la cantidad.")
+    print("Error: Por favor, ingrese un numero valido para la cantidad.")
+
