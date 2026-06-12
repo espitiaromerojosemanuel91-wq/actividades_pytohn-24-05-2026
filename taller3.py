fd362b2737ed0437255907dd76642e9abae4a187
Jose manuel espitia romero <espitiaromerojosemanuel91@gmail.com>
2026-06-12 18:36:39 -0500

    Fix typos and formatting in contact agenda7

--- taller3.py
+++ taller3.py
@@ -1,39 +1,44 @@
 # Agenda de Contactos con Diccionario
 agenda = {}
+
 while True:
-    print("\n AGENDA DE CONTACTOS ")
+    print("\nAGENDA DE CONTACTOS")
     print("1. Añadir contacto")
     print("2. Buscar contacto")
     print("3. Mostrar contactos")
     print("4. Salir")
-    opcion = input("Seleccione una opción: ")
+    
+    opcion = input("Seleccione una opcion: ")
+    
     # Añadir contacto
     if opcion == "1":
         nombre = input("Ingrese el nombre: ")
-        telefono = input("Ingrese el teléfono: ")
+        telefono = input("Ingrese el telefono: ")
         agenda[nombre] = telefono
         print("Contacto guardado correctamente")
+        
     # Buscar contacto
     elif opcion == "2":
         nombre = input("Ingrese el nombre a buscar: ")
         if nombre in agenda:
-            print("Teléfono:", agenda[nombre])
+            print("Telefono:", agenda[nombre])
         else:
             print("Contacto no encontrado")
+            
     # Mostrar contactos
     elif opcion == "3":
         print("\nLista de contactos:")
         if len(agenda) == 0:
-            print("La agenda está vacía")
+            print("La agenda esta vacia")
         else:
             for nombre, telefono in agenda.items():
-
                 print(nombre, "->", telefono)
+                
     # Salir
     elif opcion == "4":
         print("Programa finalizado")
-
         break
-    # Opción inválida
+        
+    # Opcion invalida
     else:
-        print("Opción no válida")
+        print("Opcion no valida")
