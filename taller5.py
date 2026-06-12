6df11de1a4f158d465855822092934e67c4f5184
Jose manuel espitia romero <espitiaromerojosemanuel91@gmail.com>
2026-06-12 18:43:21 -0500

    Fix typos in comments and print statements88

--- taller5.py
+++ taller5.py
@@ -1,8 +1,8 @@
-# Mini Sistema de Gestión de Inventario
+# Mini Sistema de Gestion de Inventario
 
 inventario = []
 
-# Función para agregar productos
+# Funcion para agregar productos
 def agregar_producto():
     nombre = input("Ingrese el nombre del producto: ")
     precio = float(input("Ingrese el precio del producto: "))
@@ -15,10 +15,10 @@ def agregar_producto():
     }
 
     inventario.append(producto)
-    print(" Producto agregado correctamente.\n")
+    print("Producto agregado correctamente.\n")
 
 
-# Función para realizar una venta
+# Funcion para realizar una venta
 def realizar_venta():
     nombre = input("Ingrese el nombre del producto vendido: ")
 
@@ -30,43 +30,42 @@ def realizar_venta():
 
             if cantidad_vendida <= producto["cantidad"]:
                 producto["cantidad"] -= cantidad_vendida
-                print(" Venta realizada correctamente.\n")
+                print("Venta realizada correctamente.\n")
             else:
-                print(" No hay suficiente stock.\n")
+                print("No hay suficiente stock.\n")
 
             return
 
-    print(" Producto no encontrado.\n")
+    print("Producto no encontrado.\n")
 
 
-# Función para mostrar el inventario
+# Funcion para mostrar el inventario
 def mostrar_inventario():
 
     if len(inventario) == 0:
-        print(" El inventario está vacío.\n")
+        print("El inventario esta vacio.\n")
         return
 
-    print("\n INVENTARIO ")
+    print("\nINVENTARIO")
 
     for i, producto in enumerate(inventario, start=1):
         print(f"{i}. Nombre: {producto['nombre']}")
         print(f"   Precio: ${producto['precio']}")
         print(f"   Cantidad: {producto['cantidad']}")
         
-
     print()
 
 
-# Menú principal
+# Menu principal
 while True:
 
-    print(" MENÚ INVENTARIO ")
+    print("MENU INVENTARIO")
     print("1. Agregar producto")
     print("2. Realizar venta")
     print("3. Mostrar inventario")
     print("4. Salir")
 
-    opcion = input("Seleccione una opción: ")
+    opcion = input("Seleccione una opcion: ")
 
     if opcion == "1":
         agregar_producto()
@@ -78,8 +77,8 @@ while True:
         mostrar_inventario()
 
     elif opcion == "4":
-        print(" Saliendo del sistema...")
+        print("Saliendo del sistema...")
         break
 
     else:
-        print(" Opción inválida.\n")
+        print("Opcion invalida.\n")
